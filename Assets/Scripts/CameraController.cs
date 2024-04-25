dd17640ddab4e3d88bd66a4563ce30ecf1a9ea75
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2024-04-25 16:04:56 +0100

    ...

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,11 +7,23 @@ namespace FMP.ARPG
         [SerializeField] private Transform target = null;
         [SerializeField] private Vector3 offset = new Vector3();
         [SerializeField] private float pitch = 2f;
+        RaycastHit hit;
 
         private void LateUpdate()
         {
             transform.position = target.position - offset;
             transform.LookAt(target.position + Vector3.up * pitch);
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, hit.transform.position);
         }
     }
 }
